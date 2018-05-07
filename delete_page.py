167fd667c1d4152627729906f8679cee683aac1d
copperstick6 <copperstick6@gmail.com>
2018-05-07 16:07:25 -0500

    added prospectus script

---
+++ delete_page.py
@@ -0,0 +1,12 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+pages_to_keep = [1, 2] # page numbering starts from 0
+infile = PdfFileReader('isss_prospectus.pdf', 'rb')
+output = PdfFileWriter()
+
+for i in range(infile.getNumPages()):
+    if i in pages_to_keep:
+        p = infile.getPage(i)
+        output.addPage(p)
+
+with open('isss_short_prospectus.pdf', 'wb') as f:
+    output.write(f)
